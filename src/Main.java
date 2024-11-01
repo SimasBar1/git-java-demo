ee8c24c016838365ed574279abf217e2acef5e34
Simas <simasbartkevicius@gmail.com>
2024-11-01 17:18:52 +0200

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-public class tekstu analizatorius {
+class tekstuAnalizatorius {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("iveskite teksta");
         String text = scanner.nextLine(); // vartotojo inputa iraso
         System.out.println("tekste yra raidziu: " + text.length()); // paraso kiek yra tekste simboliu
-        System.out.println(text.split());
+        System.out.println(text.split("\\s+"));
         scanner.close();
 
 
